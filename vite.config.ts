df971d9aa0d5c9cc128512947998c06c9b63c59f
aisyahnabila <aisyahnabila2.an@gmail.com>
2026-07-29 00:02:35 +0700

    build page

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: '/my-portofolio-website/',
+    base: 'aisyahnabila-portofolio-website',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
