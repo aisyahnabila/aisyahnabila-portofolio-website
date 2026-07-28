184a6120b75b2bf63d9f845ac77a937dbe644f1b
aisyahnabila <aisyahnabila2.an@gmail.com>
2026-07-28 23:31:38 +0700

    fix bug deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: './',
+    base: '/my-portofolio-website/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
